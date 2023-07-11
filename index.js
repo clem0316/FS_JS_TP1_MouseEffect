733e3c0b17f725e37d07305962cea8219d7a0425
Clément Badin <clement@MacBook-Air-de-Clement.local>
2023-07-11 15:51:37 +0200

    fin mouseEffect et corrigé

--- index.js
+++ index.js
@@ -1,28 +1,35 @@
-// Créer 3 ronds de tailles différentes (dont un qui remplacera la souris)
+// const cursor = document.querySelector(".cursor");
 
-// Ajouter un événement sur la fenetre (window) puis animer la position de ces ronds (top, left injecter "e")
+// window.addEventListener("mousemove", (e) => {
+//   cursor.style.left = e.pageX + "px";
+//   cursor.style.top = e.pageY + "px";
+// });
 
-// S'assurer que les liens sont clickables
+// const mouse1 = document.querySelector(".mouse1");
 
-// Donner un style de transparence aux 2 plus gros ronds (mix-blend-mode)
+// window.addEventListener("mousemove", (e) => {
+//   mouse1.style.left = e.pageX + "px";
+//   mouse1.style.top = e.pageY + "px";
+// });
 
-const cursor = document.querySelector(".cursor");
+// const mouse2 = document.querySelector(".mouse2");
+
+// window.addEventListener("mousemove", (e) => {
+//   mouse2.style.left = e.pageX + "px";
+//   mouse2.style.top = e.pageY + "px";
+// });
+
+// Ci-dessus, la version en déclarant chaque curseur indépendamment.
+// C-dessous, la refactorisation en ajoutant une classe commune "mouses" à nos 3 cercles de curseur
+
+const mouses = document.querySelectorAll(".mouses");
 
 window.addEventListener("mousemove", (e) => {
-  cursor.style.left = e.pageX + "px";
-  cursor.style.top = e.pageY + "px";
+  mouses.forEach((mouse) => {
+    mouse.style.left = e.pageX + "px";
+    mouse.style.top = e.pageY + "px";
+  });
 });
 
-const cursor2 = document.querySelector(".cursor2");
-
-window.addEventListener("mousemove", (e) => {
-  cursor2.style.left = e.pageX + "px";
-  cursor2.style.top = e.pageY + "px";
-});
-
-const cursor3 = document.querySelector(".cursor3");
-
-window.addEventListener("mousemove", (e) => {
-  cursor3.style.left = e.pageX + "px";
-  cursor3.style.top = e.pageY + "px";
-});
+// - NOTE 1 :Attnetion à bien différencier querySelector (qui prend la première div avec la classe "mouses") et le querySelectorAL-ll (qui va prendre toutes les div avec la classe "mouses")
+// - NOTE 2 : on utlise le forEach pour passer tous les léments ocmportant la classe "mouses". Puis on nomme un paramètre (comme on veut) qui représente chacune des div de la classe "mouses"
