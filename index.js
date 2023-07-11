c563ef07b8943942c06f8d0f08a79abb4e5d39d5
Clément Badin <clement@MacBook-Air-de-Clement.local>
2023-07-11 11:45:58 +0200

    Exercice réussi avant correction

--- index.js
+++ index.js
@@ -5,3 +5,24 @@
 // S'assurer que les liens sont clickables
 
 // Donner un style de transparence aux 2 plus gros ronds (mix-blend-mode)
+
+const cursor = document.querySelector(".cursor");
+
+window.addEventListener("mousemove", (e) => {
+  cursor.style.left = e.pageX + "px";
+  cursor.style.top = e.pageY + "px";
+});
+
+const cursor2 = document.querySelector(".cursor2");
+
+window.addEventListener("mousemove", (e) => {
+  cursor2.style.left = e.pageX + "px";
+  cursor2.style.top = e.pageY + "px";
+});
+
+const cursor3 = document.querySelector(".cursor3");
+
+window.addEventListener("mousemove", (e) => {
+  cursor3.style.left = e.pageX + "px";
+  cursor3.style.top = e.pageY + "px";
+});
